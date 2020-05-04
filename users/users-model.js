acf75c4a0b7403638015b4b60c60d516a1ebbc4a
Kiran Junnarkar <kjunnarkar@yahoo.com>
2020-05-03 20:45:45 -0700

    Completed MVP, finished all testing

--- users/users-model.js
+++ users/users-model.js
@@ -1,9 +1,19 @@
 const db = require('../database/dbConfig');
 
 module.exports = {
-    get
+    get,
+    add,
+    findBy
 }
 
 function get() {
     return db('users').select('id', 'username');
+}
+
+function add(user) {
+    return db('users').insert(user);
+}
+
+function findBy(username) {
+    return db('users').where(username);
 }
